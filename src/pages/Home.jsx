aedcdab35b03e8d4ce1a15752bef4a0fbd6dc945
Cns Pradhyumna <kvs.pradhyumna@gmail.com>
2025-08-29 23:17:23 +0530

    path changed!

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -21,7 +21,7 @@ export const HomePage = () => {
 
   useEffect(() => {
     try {
-      const modules = import.meta.glob("src/Summaries/*.md", {
+      const modules = import.meta.glob("./src/Summaries/*.md", {
         as: "raw",
         eager: true,
       });
